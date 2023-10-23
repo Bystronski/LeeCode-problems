614113362f69ac89bc1950d5f77b08f66ec6e710
Jakub <bystronski.j@wp.pl>
2023-10-23 03:49:35 +0200

    done

---
+++ Two_Sum.py
@@ -0,0 +1,15 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+     """
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i, j]
+
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
+print(twoSum([3, 3], 6))
