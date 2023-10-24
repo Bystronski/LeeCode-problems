d784c4e6c2fdd03ed1cf7c931ff24ed1e6d0fe2c
Jakub <bystronski.j@wp.pl>
2023-10-24 06:37:00 +0200

    done

---
+++ Search_Insert_Position.py
@@ -0,0 +1,24 @@
+def searchInsert(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif target > nums[mid]:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return left
+
+
+print(searchInsert([1, 3, 5, 6], 5))
+print(searchInsert([1, 3, 5, 6], 2))
+print(searchInsert([1, 3, 5, 6], 7))
